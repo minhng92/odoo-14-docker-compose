506e94e48fee866a61ff8bd76317bd00dee46d5b
Sacha34TORRES <105350812+Sacha34TORRES@users.noreply.github.com>
2022-05-19 10:00:21 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 # clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-14-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/Sacha34TORRES/odoo-14-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
